12c9a841fa2ef93188422ac41ce5168a4a635546
Suvidh <kaushiksuvidh6@gmail.com>
2024-04-30 22:48:41 +0530

    rechanged to render server link

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -18,7 +18,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authuser) {
-            const socket = io("https://chat-application-six-sigma.vercel.app/", {
+            const socket = io("https://chat-application-0z0x.onrender.com", {
                 query: {
                     userId: authuser._id
                 }
