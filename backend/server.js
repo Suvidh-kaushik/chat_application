224d29668b872517d904cf63a184f4d30495fa3c
Suvidh <kaushiksuvidh6@gmail.com>
2024-05-01 11:31:42 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
+
+
 import authroutes from "./routes/authroutes.js"
 import messageRoutes from "./routes/messageRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
@@ -8,6 +11,8 @@ import userRoutes from "./routes/userRoutes.js"
 
 import connectToMongodb from "./db/connectToMongodb.js";
 import { app, server } from "./socket/socket.js";
+
+const _dirname=path.resolve();
 
 dotenv.config();
 
@@ -19,6 +24,12 @@ app.use("/api/auth",authroutes);
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes);
 app.use(express.json());
+
+app.use(express.static(path.join(_dirname,"/frontend/dist")));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(_dirname,"frontend","dist","index.html"));
+});
 
 
 server.listen(PORT,()=>{
